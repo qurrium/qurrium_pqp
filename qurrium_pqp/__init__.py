498683829d273b72e091c9783137c6c837039758
harui2019 <harui2019@proton.me>
2026-01-30 19:48:38 +0800

    feat: import path

--- qurrium_pqp/__init__.py
+++ qurrium_pqp/__init__.py
@@ -2,3 +2,13 @@
 Predicting Properties of Quantum Many-Body Systems
 
 """
+
+from .classical_shadow import qurrium_to_pqp_result, pqp_result_to_qurrium
+from .bitwise import bitwise_core, BitWiseTraceMethod, BitWiseTraceMethodType
+from .utils import (
+    convert_to_basis_spin,
+    measurements_export,
+    measurements_read,
+    get_qurrium_version_info,
+    QURRIUM_VERSION
+)
