9ebd004cc0b596136a5334c84efb2717d487e9e9
harui2019 <harui2019@proton.me>
2026-01-30 19:47:26 +0800

    chore(boorust): remove qurrium imple. parts

---
+++ qurrium_pqp/boorust.pyi
@@ -0,0 +1,5 @@
+"""Boorust - The Rust Acceleration of Qurrium PQP (:mod:`qurrium_pqp.boorust`)
+
+Currently, this module is a placeholder for future implementations of Rust-accelerated
+functions that enhance the performance of Qurrium PQP operations.
+"""
--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,11 @@
-use indicatif::{ParallelProgressIterator, ProgressBar, ProgressStyle};
 use pyo3::prelude::*;
-use rayon::prelude::*;
-
-fn rho_elt_process(
-    rho_a_i: &String,
-    rho_b_i: &String,
-    rho_a_i1: &String,
-    rho_b_i1: &String,
-) -> f64 {
-    if rho_a_i != rho_b_i {
-        0.5
-    } else {
-        if rho_a_i1 == rho_b_i1 {
-            5.0
-        } else {
-            -4.0
-        }
-    }
-}
-
-fn get_trace(rho_a: &Vec<String>, rho_b: &Vec<String>, substring_index: &Vec<usize>) -> f64 {
-    if substring_index.is_empty() {
-        return 1.0;
-    }
-
-    substring_index.iter().fold(1.0, |acc, &i| {
-        acc * rho_elt_process(&rho_a[i], &rho_b[i], &rho_a[i + 1], &rho_b[i + 1])
-    })
-}
-
-#[pyfunction]
-fn perform_trace_calculation(data: Vec<Vec<String>>, subs: Vec<usize>) -> f64 {
-    let num_samples = data.len();
-    let substring_indices: Vec<usize> = subs.iter().map(|&i| i * 2).collect();
-
-    let bar = ProgressBar::new(num_samples as u64);
-    bar.set_style(
-        ProgressStyle::default_bar()
-            .template(
-                "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})",
-            )
-            .unwrap()
-            .progress_chars("#>-"),
-    );
-
-    let result = (0..num_samples)
-        .into_par_iter()
-        .progress_with(bar)
-        .map(|i| {
-            let row_sum: f64 = (i + 1..num_samples)
-                .into_par_iter()
-                .map(|j| get_trace(&data[i], &data[j], &substring_indices))
-                .sum();
-            row_sum
-        })
-        .sum();
-
-    result
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn dismatmul_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(perform_trace_calculation, m)?)?;
+fn boorust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
+
+// Due to the original functionality being integrated into the main module qurrium,
+// There is no other content in this module for now.
+// Keep this null module for future expansion or compatibility.
