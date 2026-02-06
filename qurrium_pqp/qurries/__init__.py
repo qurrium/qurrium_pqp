e314efff90d848188370be66dc79a8fba972b5a2
harui2019 <harui2019@proton.me>
2026-02-06 18:56:30 +0800

    fix: un-export

--- qurrium_pqp/qurries/__init__.py
+++ qurrium_pqp/qurries/__init__.py
@@ -3,7 +3,7 @@
 from .conversion import check_classical_shadow_exp, qurrium_to_pqp_result, pqp_result_to_qurrium
 from .classical_shadow_more import ShadowUnveilMore, SUMeasureArgs
 
-__export__ = [
+__all__ = [
     "check_classical_shadow_exp",
     "qurrium_to_pqp_result",
     "pqp_result_to_qurrium",
